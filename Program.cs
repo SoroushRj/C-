e3d1c97f48ac33dfe0cb731f369934e95572170e
Mohammad Reza Ranjbar <soroush.rj99@gmail.com>
2024-11-05 23:40:28 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter First Number: ");
+			int x = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter Second Number: ");
+			int y = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Average = " + ((x + y) / 2));
+			Console.WriteLine("Press Any Key To Continue...");
+			Console.ReadKey();
+		}
+	}
+}
