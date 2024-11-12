e2c87dd89aa9f7afd169b6b2ed6cf169339b253c
Mohammad Reza Ranjbar <soroush.rj99@gmail.com>
2024-11-12 11:06:18 +0330

    Add files via upload
    Homework 2

---
+++ Program2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number: ");
+            int a;
+            
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine(fact(a));
+            Console.WriteLine("Press To Continue...");
+            Console.ReadKey();
+        }
+
+        static int fact(int a)
+        {
+            int sum;
+            if (a != 0 || a > 0)
+            {
+                sum = a * fact(a - 1);
+            }
+            else
+            {
+                return 1;
+            }
+            return sum;
+        }
+    }
+}
